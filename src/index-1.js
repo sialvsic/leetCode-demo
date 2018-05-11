3e137a49d39b6225e4b9b4f07b40da7e5c0cdee9
zhou.yixing <zhou.yixing@rea-group.com>
2018-05-11 18:10:12 +0800

    init project and finish 1th exercise

---
+++ src/index-1.js
@@ -0,0 +1,17 @@
+function twoSum(nums, target) {
+
+  for(let x = 0; x < nums.length; x++) {
+    for(let y = 0; y < nums.length; y++) {
+
+      if(target === nums[x] + nums[y] && x !== y) {
+
+        return [x, y]
+      }
+    }
+  }
+}
+
+// twoSum([2, 7, 11, 15], 9);
+// twoSum([3, 2, 4], 6);
+
+export default twoSum;
---
+++ test/index-1-spec.js
@@ -0,0 +1,13 @@
+import twoSum from '../src/index-1';
+
+describe('twoSum', () => {
+
+  it('Array: [2, 7, 11, 15] target: 9, result: [0, 1]', () => {
+    expect(twoSum([2, 7, 11, 15], 9)).to.deep.equal([0, 1])
+  });
+
+  it('Array: [3, 2, 4] target: 6, result: [1, 2]', () => {
+    expect(twoSum([3, 2, 4], 6)).to.deep.equal([1, 2])
+  });
+
+});
